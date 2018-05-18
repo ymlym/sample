a576cc61ae6b0f202a4d6419cb98449df5393df9
lym <ym_lym@163.com>
2018-05-18 22:08:49 +0800

    finish user sign-up

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,8 +6,11 @@
   <body>
   	@include('layouts._header');
     <div class="container">
-      @yield('content')
-      @include('layouts._footer')
+      <div>
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
+      </div>
     </div>    
   </body>
 </html>
